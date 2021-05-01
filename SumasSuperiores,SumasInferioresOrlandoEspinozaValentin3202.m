35aa5b28b106b3a0efcd6f980f43ba01c61cd485
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-04-30 22:46:07 -0500

    Create SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m

---
+++ SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m
@@ -0,0 +1,15 @@
+% Octave Script
+% Title			:Sumatoria de Riemann_supeiores_e_inferiores.
+% Description		:Script para desarrrollar el ejericio 1 de la actividad Sumatoria de Riemann_supeiores_e_inferiores.
+% Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
+% Date			:20210429
+% sion		        :1
+% Usage			:octave> /path/SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202
+% Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
+%program execution page :https://octave-online.net
+ 
+fprintf ('El  AREA del ejercicio es 27u^2')
+fprintf ('Grafica')
+x=1:0.01:4;
+y=(x.^2+2);
+plot(x,y)
