75ca335e3ee5b8354b126e65b467d80b829dd63d
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-04-30 23:45:17 -0500

    Update Ej1SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m

--- Ej1SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m
+++ Ej1SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m
@@ -8,51 +8,8 @@
 % Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
 %program execution page :https://octave-online.net
  
-syms x 
-f = @(x) x^2 + 2
-%intervalos
-desde = 1
-hasta = 4
-%numero de rectangulos
-cant = 40
-h = (hasta - desde)/cant
-%calculando xi
-xi = linspace(desde, hasta, cant+2)
-%almacenamiento de x 
-for i = 1 : cant+1
-    yi(i) = f(xi(i));
-end
-%mostrar en pantalla   
-Rn = h* sum(double(yi(1 : cant)))
-Ln = h* sum(double(yi(2 : cant+1)))
-
-ezplot(f,[desde hasta]);
-hold on;
-
-plot([xi(1) xi(end)],[0 0],'b')
-
-for i = 1 : cant+1
-    if(f(xi(i)) >= 0)
-
-        plot([xi(i) xi(i)],[0 double(f(fminbnd(f,xi(i),xi(i+1))))],'g')
-        plot([xi(i) xi(i+1)],[double(f(fminbnd(f,xi(i),xi(i+1)))) double(f(fminbnd(f,xi(i),xi(i+1))))],'g')
-        plot([xi(i+1) xi(i+1)],[double(f(fminbnd(f,xi(i),xi(i+1)))) 0],'g')
-    
-        g = @(x) -f(x);
-    
-        plot([xi(i) xi(i)],[0 double(f(fminbnd(g,xi(i),xi(i+1))))],'r')
-        plot([xi(i) xi(i+1)],[double(f(fminbnd(g,xi(i),xi(i+1)))) double(f(fminbnd(g,xi(i),xi(i+1))))],'r')
-        plot([xi(i+1) xi(i+1)],[double(f(fminbnd(g,xi(i),xi(i+1)))) 0],'r')
-    else
-
-        plot([xi(i) xi(i)],[0 double(f(fminbnd(f,xi(i),xi(i+1))))],'r')
-        plot([xi(i) xi(i+1)],[double(f(fminbnd(f,xi(i),xi(i+1)))) double(f(fminbnd(f,xi(i),xi(i+1))))],'r')
-        plot([xi(i+1) xi(i+1)],[double(f(fminbnd(f,xi(i),xi(i+1)))) 0],'r')
-    
-        g = @(x) -f(x);
-    
-        plot([xi(i) xi(i)],[0 double(f(fminbnd(g,xi(i),xi(i+1))))],'g')
-        plot([xi(i) xi(i+1)],[double(f(fminbnd(g,xi(i),xi(i+1)))) double(f(fminbnd(g,xi(i),xi(i+1))))],'g')
-        plot([xi(i+1) xi(i+1)],[double(f(fminbnd(g,xi(i),xi(i+1)))) 0],'g')
-    end
-end
+fprintf ('El  AREA del ejercicio es 27u^2')
+fprintf ('Grafica')
+x=1:0.01:4;
+y=(x.^2+2);
+plot(x,y)
