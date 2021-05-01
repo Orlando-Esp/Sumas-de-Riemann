12e2b9fccd567a0e58221f75774b481451eff245
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-04-30 22:58:18 -0500

    Create Ej3SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m

---
+++ Ej3SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202.m
@@ -0,0 +1,15 @@
+% Octave Script
+% Title			:Sumatoria de Riemann_supeiores_e_inferiores.
+% Description		:Script para desarrrollar el ejericio 3 de la actividad Sumatoria de Riemann_supeiores_e_inferiores.
+% Author		:Orlando Espinoza Valentin (Orlando-Esp) espinozaorlando978@gmail.com
+% Date			:20210429
+% sion		        :1
+% Usage			:octave> /path/SumasSuperiores,SumasInferioresOrlandoEspinozaValentin3202
+% Notes			:Requiere aplicacion octave-online usar en consola preferentemente. 
+%program execution page :https://octave-online.net
+clear
+fprintf ('El  AREA del tercer ejercicio es 13/2u^2')
+fprintf ('Grafica')
+x=-2:0.01:3;
+y=(-x+1);
+plot(x,y)
